c6d7c9cce2519bb0ff05eb7b7fb124a5d4f01fa5
Aleksandr-Bryzgalin <aleksandr.bryzgalin@hotmail.com>
2021-06-24 17:12:09 +0300

    добавляет работу  переключателя цвета

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-];
+import colors from './colors.js';
+
+const INTERVALTIME = 1000;
+let intervalId = null;
+let isActive = false;
+
+const refs = {
+  startBtnRef: document.querySelector('[data-action="start"]'),
+  stopBtnRef: document.querySelector('[data-action="stop"]'),
+  bodyRef: document.querySelector('body'),
+}
+
+refs.startBtnRef.addEventListener('click', onStartChangeBodyColor)
+refs.stopBtnRef.addEventListener('click', onStopChangeBodyColor)
+
+function onStartChangeBodyColor() {
+  if (!isActive) {
+    isActive = true;
+    intervalId = setInterval(() => {      
+      refs.bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+      console.log('HLEB Start')
+    }, INTERVALTIME)
+  }  
+};
+
+function onStopChangeBodyColor() {
+  clearInterval(intervalId)
+  isActive = false;
+  console.log('CLICK STOP');
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
-
